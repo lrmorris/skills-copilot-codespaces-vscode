eb2c2b8d9ee0824493ae615cf75e7d42bb15abed
lrmorris <151921210+lrmorris@users.noreply.github.com>
2023-12-13 15:06:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,41 +1,30 @@
-//create web server
-const express = require('express');
-const app = express();
-//create a port
-const port = process.env.PORT || 3000;
-//create a json object
-app.use(express.json());
-//create an array of objects
-const comments = [
-    { username: 'Tammy', comment: 'lololol' },
-    { username: 'FishBoi', comment: 'Nice one' },
-    { username: 'Hans', comment: 'Cool' },
-    { username: 'Derek', comment: 'I love it' },
-    { username: 'Bobby', comment: 'Yikes' }
-];
-//create a get request
-app.get('/comments', (req, res) => {
-    res.send(comments);
-});
-//create a post request
-app.post('/comments', (req, res) => {
-    const newComment = { username: req.body.username, comment: req.body.comment };
-    comments.push(newComment);
-    res.send(newComment);
-});
-//create a delete request
-app.delete('/comments/:index', (req, res) => {
-    const deletedComment = comments.splice(req.params.index, 1);
-    res.send(deletedComment);
-});
-//create a put request
-app.put('/comments/:index', (req, res) => {
-    const updatedComment = req.body.comment;
-    comments[req.params.index].comment = updatedComment;
-    res.send(updatedComment);
-});
-//create a listen request
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});  
+// Create web server
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Create express app
+const app = express();
+
+// Middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Route
+app.get('/posts/:id/comments', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            content: 'This is a comment',
+            postId: req.params.id
+        }
+    ]);
+});
+
+// Start server
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
+});
